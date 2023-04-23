91bedee15c1b14d3c009a9d5855b71cc268da7ed
hong-sile <gurwns9325@naver.com>
2023-04-23 17:09:03 +0900

    feat: RequestBody가 Dto의 값에 역직렬화가 되지 않는 케이스에 대해 예외처리

--- src/main/java/racingcar/controller/RacingCarControllerAdvice.java
+++ src/main/java/racingcar/controller/RacingCarControllerAdvice.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,7 @@ public class RacingCarControllerAdvice {
     private static final String UNEXPECTED_ERROR_LOG_FORMAT = "예상치 못한 에러 발생 : " + System.lineSeparator() + "{}";
     //좀 더 적절한 메시지가 있을지 고민해보기.
     private static final String UNEXPECTED_ERROR_MESSAGE = "네트워크 지연으로 다시 시도해주시기 바랍니다.";
+    private static final String CANNOT_DESERIALIZE_REQUEST_ERROR_MESSAGE = "타입에 맞는 값을 입력해주시길 바랍니다.";
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(RuntimeException.class)
@@ -38,6 +40,11 @@ public class RacingCarControllerAdvice {
         ex.getBindingResult().getAllErrors()
                 .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handle(final HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(CANNOT_DESERIALIZE_REQUEST_ERROR_MESSAGE);
     }
 
     private String convertToString(final Exception e) {
